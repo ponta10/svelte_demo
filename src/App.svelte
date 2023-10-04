029ebda1bcc145f17184dd1010364013a16bd45a
ponta10 <mponta2002@gmail.com>
2023-10-04 14:29:01 +0900

    todo作成

--- src/App.svelte
+++ src/App.svelte
@@ -1,36 +1,61 @@
 <script lang="ts">
-	export let name: string;
+  export let name: string = "world";
 
-	let countStore: number = 0;
-	function count() {
-		return ++countStore;
-	}
+  let newTask: string = "";
+  let tasks: string[] = [];
+
+  function addTask() {
+    if (newTask.trim() !== "") {
+      tasks = [...tasks, newTask.trim()];
+      newTask = "";
+    }
+  }
+
+  function removeTask(index: number) {
+    tasks = tasks.filter((_, i) => i !== index);
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-	<button on:click="{count}">{ countStore }</button>
+  <h1>Hello {name}!</h1>
+  <input bind:value={newTask} placeholder="New task" />
+  <button on:click={addTask}>Add task</button>
+
+  <ul>
+    {#each tasks as task, index}
+      <li>
+        <button on:click={() => removeTask(index)}>{task}</button>
+      </li>
+    {/each}
+  </ul>
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    margin: 0.5em 0;
+    cursor: pointer;
+  }
+
+  li:hover {
+    text-decoration: line-through;
+  }
 </style>
